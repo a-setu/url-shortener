e5615e689a613d04eb5446d6432b89bd009ef970
Faisal Memon <faisal.memon1991@gmail.com>
2024-12-29 16:29:06 +0530

    Create New Short Url Functionality

--- url-shortener-sb/src/main/java/com/url/shortener/security/WebSecurityConfig.java
+++ url-shortener-sb/src/main/java/com/url/shortener/security/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        //.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/urls/**").authenticated()
                         .requestMatchers("/{shortUrl}").permitAll()
